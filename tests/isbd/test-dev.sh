e324e938fb74be722ecf35a06a9a1dda4aa7a7ea
lromeraj <lromerajdev@gmail.com>
2023-03-27 00:12:35 +0200

    WIP Project clean

--- tests/isbd/test-dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# export QEMU_EXTRA_FLAGS="-chardev pty,id=con1,mux=on -serial chardev:con1"
-
-rm -rf twister-out*
-twister -vv -p qemu_cortex_m3 -T . -W
-
-# twister -vv -p qemu_cortex_m3 -T . -W
-# twister -vv  -p frdm_k64f -T . -W --device-testing --device-serial /dev/ttyACM0 --west-flash
