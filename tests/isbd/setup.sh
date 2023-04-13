02685de79459baca4d3642fe35740018431b09f5
lromeraj <lromerajdev@gmail.com>
2023-04-13 02:33:15 +0200

    FIX Tests
    Iridium SBD emulator updated to the latest version

--- tests/isbd/setup.sh
+++ tests/isbd/setup.sh
@@ -10,10 +10,10 @@ PID_FILE_PATH="/tmp/isbd.tests.pids"
 ISBD_EMULATOR_PATH="${WDIR}/iridium-sbd-emulator"
 
 # build emulator
-npm --prefix $ISBD_EMULATOR_PATH run build
+npm --prefix $ISBD_EMULATOR_PATH install
 
 fg_clear $PID_FILE_PATH # clear old pids
 fg_create_tty $PID_FILE_PATH $1 $TTY_S1_PATH
 
 fg_process $PID_FILE_PATH \
-  node $ISBD_EMULATOR_PATH/build/emu.js --path $TTY_S1_PATH
+  node $ISBD_EMULATOR_PATH/960x.js --path $TTY_S1_PATH
