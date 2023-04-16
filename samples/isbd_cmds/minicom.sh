10f0fc12fa6151f008d2c7527387c6f77c7bb6a5
lromeraj <lromerajdev@gmail.com>
2023-04-16 18:15:13 +0200

    FIX Module

---
+++ samples/isbd_cmds/minicom.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+minicom -D /dev/ttyACM0 -b 115200
+#minicom -D /dev/ttyUSB0 -b 19200
--- samples/isbd_cmds/src/message.h
+++
@@ -1,7 +0,0 @@
-#ifndef MESSAGE_H_
-  #define MESSAGE_H_
-  
-  void msg_setup();
-  void msg_enqueue( const uint8_t *msg, uint16_t msg_len );
-
-#endif
