dfa35fd138d3bcd58b583d7787fc10e75036b452
lromeraj <lromerajdev@gmail.com>
2023-06-12 13:32:29 +0200

    FIX ISU Docs

--- lib/iridium/inc/isu.h
+++ lib/iridium/inc/isu.h
@@ -94,26 +94,26 @@
   /**
    * @brief Query the device for Iridium system time if available
    * 
-   * @param __rtc 
-   * @return int8_t 
+   * @param rtc Output buffer
+   * @return int8_t Output buffer length
    */
-  isu_dte_err_t isu_get_rtc( isu_dte_t *isbd, char *__rtc , size_t rtc_len );
+  isu_dte_err_t isu_get_rtc( isu_dte_t *isbd, char *rtc , size_t rtc_len );
 
   /**
    * @brief Query the device IMEI
    * 
-   * @param __imei Resulting IMEI memory buffer
-   * @return int8_t
+   * @param imei Resulting IMEI memory buffer
+   * @return int8_t Output buffer size
    */          
-  isu_dte_err_t isu_get_imei( isu_dte_t *dte, char *__imei, size_t imei_len );
+  isu_dte_err_t isu_get_imei( isu_dte_t *dte, char *imei, size_t imei_len );
 
   /**
    * @brief Query the device revision
    * 
-   * @param __revision 
-   * @return int8_t 
+   * @param rev Output revision buffer 
+   * @return int8_t Output buffer size
    */
-  isu_dte_err_t isu_get_revision( isu_dte_t *dte, char *__rev, size_t rev_len );
+  isu_dte_err_t isu_get_revision( isu_dte_t *dte, char *rev, size_t rev_len );
 
   /**
    * @brief Transfer a SBD text message from the DTE 
@@ -143,21 +143,27 @@
   isu_dte_err_t isu_mo_to_mt( isu_dte_t *dte, char *out, uint16_t out_len );
 
   /**
-   * @brief 
+   * @brief This command is used to transfer a binary SBD 
+   * message from the single mobile terminated buffer in the
+   * 9602 to the DTE. The mobile terminated buffer can contain 
+   * only one message at any one time
    * 
-   * @param msg 
-   * @param msg_len 
-   * @param csum 
-   * @return dte_err_t 
+   * @param msg Output message buffer
+   * @param msg_len Output message buffer size
+   * @param csum MT message checksum
+   * @return isu_dte_err_t
    */
   isu_dte_err_t isu_get_mt( isu_dte_t *dte, uint8_t *msg, uint16_t *msg_len, uint16_t *csum );
 
   /**
-   * @brief 
+   * @brief This command is used to transfer a binary SBD 
+   * message from the DTE to the single mobile originated buffer 
+   * in the 9602. The mobile originated buffer can contain only 
+   * one message at any one time.
    * 
-   * @param __msg 
-   * @param msg_len 
-   * @return int8_t 
+   * @param msg MO message buffer
+   * @param msg_len MO message buffer size
+   * @return isu_dte_err_t
    */
   isu_dte_err_t isu_set_mo( isu_dte_t *dte, const uint8_t *msg, uint16_t msg_len );
 
@@ -169,32 +175,35 @@
    * a human friendly interface to SBD for demonstrations and application development. 
    * It is expected that most usage of SBD will be with binary messages.
    *  
-   * @param __mt_buff 
-   * @return int8_t 
+   * @param mt_buff MT message output buffer
+   * @return isu_dte_err_t
    */
-  isu_dte_err_t isu_get_mt_txt( isu_dte_t *dte, char *__mt_buff, size_t mt_buff_len );
+  isu_dte_err_t isu_get_mt_txt( isu_dte_t *dte, char *mt_buff, size_t mt_buff_len );
 
   /**
-   * @brief 
+   * @brief This command initiates an SBD session between the 9602 and the GSS
    * 
-   * @param session 
-   * @return int8_t 
+   * @param session Output session struct
+   * @param alert Session is in response of a previously received alert
+   * @return isu_dte_err_t
    */
   isu_dte_err_t isu_init_session( isu_dte_t *dte, isu_session_ext_t *session, bool alert );
 
   /**
-   * @brief 
+   * @brief This command is used to clear the mobile originated buffer, 
+   * mobile terminated buffer or both
    * 
-   * @param session 
-   * @return int8_t 
+   * @param buffer The buffer or buffers that should be cleared
+   * @return isu_dte_err_t
    */
   isu_dte_err_t isu_clear_buffer( isu_dte_t *dte, isu_clear_buffer_t buffer );
 
   /**
-   * @brief Execution command returns the received signal strength indication <rssi> from the 9602.
+   * @brief Execution command returns the received signal 
+   * strength indication RSSI from the 9602.
    * 
-   * @param signal 
-   * @return int8_t 
+   * @param signal Output signal strength
+   * @return isu_dte_err_t 
    */
   isu_dte_err_t isu_get_sig_q( isu_dte_t *dte, uint8_t *signal );
 
@@ -214,16 +223,16 @@
   /**
    * @brief Enable or disable the ISU to listen for SBD Ring Alerts
    *  
-   * @param alert 
-   * @return dte_err_t 
+   * @param alert Alert option
+   * @return isu_dte_err_t 
    */
   isu_dte_err_t isu_set_mt_alert( isu_dte_t *dte, isu_mt_alert_t alert );
 
   /**
    * @brief Query the current ring indication mode
    * 
-   * @param alert 
-   * @return dte_err_t 
+   * @param alert Output for current ring indication mode
+   * @return isu_dte_err_t
    */
   isu_dte_err_t isu_get_mt_alert( isu_dte_t *dte, isu_mt_alert_t *alert );
 
@@ -231,7 +240,7 @@
    * @brief Triggers an SBD session to perform a manual SBD Network Registration
    * 
    * @param reg_sts A pointer where the resulting registration status will be stored 
-   * @return dte_err_t 
+   * @return isu_dte_err_t 
    */
   isu_dte_err_t isu_net_reg( isu_dte_t *dte, isu_net_reg_sts_t *reg_sts );
 
@@ -239,8 +248,8 @@
    * @brief Query the ring indication status, returning the reason 
    * for the most recent assertion of the Ring Indicator
    * 
-   * @param ring_sts
-   * @return dte_err_t
+   * @param ring_sts Output for current ring status
+   * @return isu_dte_err_t
    */
   isu_dte_err_t isu_get_ring_sts( isu_dte_t *dte, isu_ring_sts_t *ring_sts );
 
